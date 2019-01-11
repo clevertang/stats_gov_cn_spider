8558aa4f5b2e5dea571fd00c8bfb4ce0411c7b0d
tangxin <961577196@qq.com>
2019-01-11 18:58:55 +0800

    update  this first version

---
+++ settings.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+"""
+@version: python2.7
+@author: ‘xin.tang‘
+@license: Apache Licence 
+@software: PyCharm
+@file: settings.py
+@time: 2019/1/11 6:25 PM
+"""
+
+INDEX_URL = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html'
+
+DATABASE = {
+	'host': 'localhost',
+	'prot': 3306,
+	'user': 'root',
+	'password': 'tx123321z',
+	'db': 'test'
+}
+HOST='http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017'
